0fcc574455e2c26e974a2b45d81ab804006da174
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-07-20 20:54:16 +0100

    smile cascade created to recognise and highlight smiles within a picture

--- work/src/faces.py
+++ work/src/faces.py
@@ -4,6 +4,7 @@ import pickle
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
 
 recogniser = cv2.face.LBPHFaceRecognizer_create()
 recogniser.read('trainer.yml')
@@ -27,7 +28,8 @@ while True:
 
         #recogniser
         id_, conf = recogniser.predict(roi_grey)
-        if conf >= 45 and conf <= 85:
+        if conf >= 45:
+            print(labels[id_])
             font = cv2.FONT_HERSHEY_SIMPLEX
             name = labels[id_]
             colour = (255,255,255)
@@ -44,9 +46,13 @@ while True:
         end_cord_y = y + h
         cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), colour, stroke)
         eyes = eye_cascade.detectMultiScale(roi_grey)
+        smile = smile_cascade.detectMultiScale(roi_grey)
 
         for(ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_colour,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        
+        for(sx,sy,sw,sh) in smile:
+            cv2.rectangle(roi_colour,(sx,sy),(sx+sw,sy+sh),(255,0,0),2)
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
